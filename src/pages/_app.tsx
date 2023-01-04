0a8c9ef0574136734ba988f94dcfa82b6d609571
Adrian Gajda <adrian.gajda@hotmail.com>
2023-01-04 11:14:50 +0000

    setup dependencies

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import React from "react";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
