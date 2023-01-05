4b2dad3b47af97daef893cc1a2dbcddae217366c
Adrian Gajda <adrian.gajda@hotmail.com>
2023-01-05 16:54:14 +0000

    updates

--- src/lib/oauth/oauthUtils.ts
+++ src/lib/oauth/oauthUtils.ts
@@ -1,8 +1,6 @@
 import { Ref } from "react";
 
-const OAUTH_STATE_KEY = "react-use-oauth2-state-key";
-const POPUP_HEIGHT = 700;
-const POPUP_WIDTH = 600;
+
 
 export const generateOAuthState = () => {
   let possible =
@@ -14,29 +12,4 @@ export const generateOAuthState = () => {
 
   console.log(text);
   return text;
-};
-
-export const saveState = (state: string) => {
-  console.log("state saved");
-  sessionStorage.setItem("test", state);
-};
-
-export const removeState = () => {
-  sessionStorage.removeItem(OAUTH_STATE_KEY);
-};
-
-export const openPopup = (url: string) => {
-  // To fix issues with window.screen in multi-monitor setups, the easier option is to
-  // center the pop-up over the parent window.
-  const top = window.outerHeight / 2 + window.screenY - POPUP_HEIGHT / 2;
-  const left = window.outerWidth / 2 + window.screenX - POPUP_WIDTH / 2;
-  return window.open(
-    url,
-    "OAuth2 Popup",
-    `height=${POPUP_HEIGHT},width=${POPUP_WIDTH},top=${top},left=${left}`
-  );
-};
-
-export const closePopup = (popupRef: HTMLElement) => {
-  popupRef.current?.close();
 };
