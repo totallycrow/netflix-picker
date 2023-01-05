d3a8c11dbb0a296fca8c8b17bcc9eadf388f51d2
Adrian Gajda <adrian.gajda@hotmail.com>
2023-01-05 10:49:30 +0000

    scrap next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,9 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
-  console.log(session);
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>
+      <h1>test</h1>
+    </div>
   );
 }
