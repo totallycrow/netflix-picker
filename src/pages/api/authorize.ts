414d1eeba763d3aaa988df049391a6e842663212
Adrian Gajda <adrian.gajda@hotmail.com>
2023-01-06 16:16:39 +0000

    sketch auth flow further

--- src/pages/api/authorize.ts
+++
@@ -1,8 +0,0 @@
-// https://api.themoviedb.org/3/authentication/token/new?api_key=0813f3326aa955f3707a6e8d13d652f7
-const url = require("url");
-
-export default async function handler(req, res) {
-  console.log(req.url);
-  await setTimeout(() => console.log("test", 1000));
-  res.redirect(307, "/");
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   const [token, setToken] = useState("");
   const [sessionState, setSessionState] = useState("");
   const [isAuthorized, setIsAuthorized] = useState(0);
+  const [test, setTest] = useState("");
   // generateOAuthState();
 
   // if (typeof window !== "undefined") {
@@ -14,6 +15,15 @@ export default function Home() {
   //   const top = window.outerHeight;
   //   console.log(top);
   // }
+
+  useEffect(() => {
+    const cookie = localStorage.getItem("sessionAccess");
+    if (cookie === null) return;
+    const val = JSON.parse(cookie);
+
+    if (val.sessionID.length === 40 && val.successfull === true)
+      setIsAuthorized(1);
+  }, [isAuthorized]);
 
   const handleAuth = async () => {
     const token = await fetch("./api/token").then((res) => res.json());
@@ -29,8 +39,34 @@ export default function Home() {
     setSessionState(ses);
     localStorage.setItem("sessionID", ses);
 
-    window.location.href = `https://www.themoviedb.org/authenticate/${token.token}?redirect_to=http://localhost:3000?${ses}`;
+    window.location.href = `https://www.themoviedb.org/authenticate/${token.token}?redirect_to=http://localhost:3000/auth?stateId=${ses}`;
   };
+
+  const handleLogout = () => {
+    localStorage.removeItem("sessionAccess");
+    setIsAuthorized(0);
+  };
+
+  // https://api.themoviedb.org/3/account?api_key=0813f3326aa955f3707a6e8d13d652f7&session_id=xxx
+  useEffect(() => {
+    if (!isAuthorized) {
+      setTest("");
+      return;
+    }
+
+    const getUserData = async () => {
+      const x = localStorage.getItem("sessionAccess");
+      console.log(x);
+      const stateId = JSON.parse(x).sessionID;
+
+      const data = await fetch(
+        `https://api.themoviedb.org/3/account?api_key=0813f3326aa955f3707a6e8d13d652f7&session_id=${stateId}`
+      ).then((res) => res.json());
+      console.log(data);
+      setTest(data.username);
+    };
+    getUserData();
+  }, [isAuthorized]);
 
   return (
     <div>
@@ -38,7 +74,10 @@ export default function Home() {
       <div>Token: {token}</div>
       <div>Session ID: {sessionState}</div>
       <div>Is Authorised? {isAuthorized}</div>
+      <div>Test: {test}</div>
       <button onClick={handleAuth}>Login with IMDB</button>
+      <div></div>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
