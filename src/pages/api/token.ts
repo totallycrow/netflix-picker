f3ddf6a3fdd7ed011fa48c9f0e3951a2bd00ec8d
Adrian Gajda <adrian.gajda@hotmail.com>
2023-01-11 16:53:02 +0000

    refactor data fetching

--- src/pages/api/token.ts
+++ src/pages/api/token.ts
@@ -1,6 +1,9 @@
 // https://api.themoviedb.org/3/authentication/token/new?api_key=0813f3326aa955f3707a6e8d13d652f7
 
+import moviesAPI from "../../services/tmdb/moviesAPI";
+
 export default async function handler(req, res) {
+  const handler = moviesAPI.getToken();
   const token = await fetch(
     "https://api.themoviedb.org/3/authentication/token/new?api_key=0813f3326aa955f3707a6e8d13d652f7"
   ).then((response) => response.json());
