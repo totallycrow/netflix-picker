b0fd3d7886efd1d7bd5cce341a5111876eb74620
Adrian Gajda <adrian.gajda@hotmail.com>
2023-01-24 11:25:10 +0000

    sketch out modal on hash errors with chakraui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,26 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import React, { useEffect } from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Button, ChakraProvider, useDisclosure } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   // const {openModal, isModalOpened} = useModal()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const onHashChangeStart = (url: any) => {
@@ -25,7 +36,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       console.log(`Path changing to ${router.asPath}`);
 
       if (url.includes("#")) {
-        alert("Test");
+        // alert("Test");
+        setError(
+          "Error Code: 127. Cannot load the requested data. Please contact the support."
+        );
+        onOpen();
       }
 
       // openModal
@@ -44,6 +59,21 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Error</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>{error}</ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              {/* <Button variant="ghost">Secondary Action</Button> */}
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
         <Hydrate state={pageProps.dehydratedState}>
           {/* <ErrorModalProvider value={{openModal, isModalOpened}}> */}
           <Component {...pageProps} />
